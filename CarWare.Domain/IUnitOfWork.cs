0fc7f102b6002f497b6565279406dcdd6956bc6b
Abeer Reda <abeerreda07@gmail.com>
2025-11-13 22:24:51 +0200

    Add Unit of Work pattern implementation

---
+++ CarWare.Domain/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CarWare.Domain.Entities;
+using CarWare.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWare.Domain
+{
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> CompleteAsync();
+    }
+}
---
+++ CarWare.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using CarWare.Domain;
+using CarWare.Domain.Entities;
+using CarWare.Domain.Interfaces;
+using CarWare.Infrastructure.Context;
+using CarWare.Infrastructure.Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWare.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private Hashtable _repositories;
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _repositories=new Hashtable();
+        }
+        public async Task<int> CompleteAsync()
+        => await _dbContext.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync()
+        => await _dbContext.DisposeAsync();
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+           var key=typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(key))
+            {
+                var repository=new GenericRepository<TEntity>(_dbContext);
+                _repositories.Add(key, repository);
+            }
+            return _repositories[key] as IGenericRepository<TEntity>;
+        }
+    }
+}
