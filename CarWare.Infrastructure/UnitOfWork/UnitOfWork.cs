fd0d023af728318708b5f33c46b8d91260dba65a
Abdo Mohamed <abdoAbdelAziz@gamil.com>
2025-11-14 18:58:09 +0200

    put unit of word class in folder

--- CarWare.Infrastructure/UnitOfWork/UnitOfWork.cs
+++ CarWare.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarWare.Infrastructure
+namespace CarWare.Infrastructure.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
