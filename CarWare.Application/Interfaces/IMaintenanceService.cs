50e28498d68c50f348f81590d07bec7ec5557c1c
Abeer Reda <abeerreda07@gmail.com>
2025-11-25 16:45:58 +0200

    dded MaintenanceDto for data transfer - Created IMaintenanceRepository and MaintenanceRepository with IQueryable getters - Added IMaintenanceService interface with CRUD and paginated GETs

---
+++ CarWare.Application/Interfaces/IMaintenanceService.cs
@@ -0,0 +1,20 @@
+﻿using CarWare.Application.Common;
+using CarWare.Application.DTOs.Maintenance;
+using CarWare.Domain.helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWare.Application.Interfaces
+{
+    public interface IMaintenanceService
+    {
+        Task<MaintenanceDto> AddAsync(MaintenanceDto dto);
+        Task<Pagination<MaintenanceDto>> GetByVehicleIdAsync(int vehicleId, PaginationParameters pagination);
+        Task<Pagination<MaintenanceDto>> GetUpcomingAsync(PaginationParameters pagination);
+        Task<MaintenanceDto?> UpdateAsync(int id, MaintenanceDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
