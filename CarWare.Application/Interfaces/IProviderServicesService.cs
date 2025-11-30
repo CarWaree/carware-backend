102bb5a8ac42979dc541bd3822ef3827014a49e3
Abeer Reda <abeerreda07@gmail.com>
2025-11-30 18:30:47 +0200

    Created ProviderServiceDto to map ProviderServices entity. - Added IProviderServicesService interface with GetAllAsync method. - Implemented ProviderServicesService with GetAllAsync using AutoMapper. - Added ProviderServicesController with GET endpoint to fetch all ProviderServices. - Added filtering method GetProvidersByServiceTypeAsync in Service and Controller to get providers by MaintenanceType.

---
+++ CarWare.Application/Interfaces/IProviderServicesService.cs
@@ -0,0 +1,16 @@
+﻿using CarWare.Application.Common;
+using CarWare.Application.DTOs.Maintenance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWare.Application.Interfaces
+{
+    public interface IProviderServicesService
+    {
+        Task<Result<IEnumerable<ProviderServiceDto>>> GetAllAsync();
+        Task<Result<IEnumerable<ProviderServiceDto>>> GetProvidersByServiceTypeAsync(int serviceTypeId);
+    }
+}
