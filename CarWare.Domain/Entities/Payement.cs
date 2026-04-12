ed86c1d8ebb91424144fd8e3a407b3dd9fbd6771
Abdo Mohamed <abdoAbdelAziz@gamil.com>
2026-04-13 00:38:27 +0200

    feat(Payment): implement Payement Entity and database configuration

---
+++ CarWare.Domain/Entities/Payement.cs
@@ -0,0 +1,26 @@
+﻿using CarWare.Domain.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarWare.Domain.Entities
+{
+    public class Payment : BaseEntity
+    {
+        //FK
+        public int AppointmentId { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Amount { get; set; }
+
+        public PaymentMethod Method { get; set; }
+
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+
+        public string? TransactionId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation
+        public Appointment Appointment { get; set; }
+    }
+}
